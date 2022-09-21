0cbba58b8fb1740376c882614ffbd9e3b13d8a71
Israel Guillermo <learsi.guil2010@gmail.com>
2022-09-21 07:19:01 -0400

    Fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'http://localhost:4701',
+  url: 'https://161.35.119.130:4701',
   usuario: "kiosco1",
   password: "77WmJQA3jK1BkXD7qWrE"
 };
